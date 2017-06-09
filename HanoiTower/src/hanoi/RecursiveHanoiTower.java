6df72508bd7decd8c174f16bcbf45b586d474f62
Claudio Henrique <Claudio_Henrique89@hotmail.com>
2017-06-08 21:56:08 -0300

    Estrutura de Hanoi

---
+++ HanoiTower/src/hanoi/RecursiveHanoiTower.java
@@ -0,0 +1,20 @@
+package hanoi;
+
+public class RecursiveHanoiTower {
+	
+	//Recursividade Hanoi com pilha.
+	
+	private void move(int source, int destination){
+		System.out.println(source + "->" + destination );
+	}
+	
+	public void executeHanoi(int n,int source, int destination, int auxiliar){
+		
+		if(n > 0){
+			executeHanoi(n - 1, source, auxiliar, destination);
+			move(source, destination);
+			executeHanoi(n - 1, auxiliar, destination, source);
+			
+		}
+	}
+}
